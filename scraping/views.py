44110119565b6f7175b5dd48ef76ff63552e4a12
IhorYu <ihoryukhnovskyi@gmail.com>
2022-09-16 01:40:06 +0200

    form added

--- scraping/views.py
+++ scraping/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
+
+from .forms import FindForm
 from .models import Vacancy
 
 
 def home(request):
-    qs = Vacancy.objects.all()
-    return render(request, 'scraping/home.html', {'object_list': qs})
+    form = FindForm()
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    qs = []
+    _filter = {}
+    if city or language:
+        if city:
+            _filter['city__slug'] = city
+        if language:
+            _filter['language__slug'] = language
 
-
+    qs = Vacancy.objects.filter(**_filter)
+    return render(request, 'scraping/home.html', {'object_list': qs,
+                                                  'form': form})
